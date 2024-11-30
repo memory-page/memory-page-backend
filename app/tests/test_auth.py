ea2b4f81cf360fd080f000297e97d920eaf29db7
mseo39 <a01063294905@gmail.com>
2024-11-30 17:15:56 +0900

    test: 로그인 실패했을 경우 테스트

--- app/tests/test_auth.py
+++ app/tests/test_auth.py
@@ -15,3 +15,29 @@ async def test_login_success() -> None:
     assert response.status_code == status.HTTP_200_OK
     assert login_response.status_code == status.HTTP_200_OK
     assert "access_token" in login_response.json()["data"]
+
+
+async def test_login_fail_password() -> None:
+    # Given
+    response, mock_board_dto = await mock_create_board()
+
+    # When
+    # 비밀번호는 uuid로 생성하므로 겹칠 수 없는 값으로 지정함
+    login_response = await mock_login(board_name=mock_board_dto.board_name, password="11111111")
+        
+    # Then
+    assert response.status_code == status.HTTP_200_OK
+    assert login_response.status_code == status.HTTP_401_UNAUTHORIZED
+
+
+async def test_login_fail_board_name() -> None:
+    # Given
+    response, mock_board_dto = await mock_create_board()
+
+    # When
+    # 비밀번호는 uuid로 생성하므로 겹칠 수 없는 값으로 지정함
+    login_response = await mock_login(board_name="11111111", password=mock_board_dto.password)
+        
+    # Then
+    assert response.status_code == status.HTTP_200_OK
+    assert login_response.status_code == status.HTTP_401_UNAUTHORIZED
