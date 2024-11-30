d18dafab0734ed12046f6fd29decb1a8f0a65167
mseo39 <a01063294905@gmail.com>
2024-11-30 16:41:42 +0900

    fix: mock_login 반환값 변경

--- app/tests/mock/mock_auth.py
+++ app/tests/mock/mock_auth.py
@@ -1,20 +1,12 @@
-import dataclasses
 from httpx import AsyncClient, ASGITransport, Response
-from typing import Tuple
 
 from app.main import app
 
 
-@dataclasses.dataclass(kw_only=True, frozen=True)
-class login_dto:
-    board_id: str
-    access_token: str
-    
-
 async def mock_login(
     board_name: str, 
     password: str
-) -> Tuple[Response, login_dto]:
+) -> Response:
     
     login_request = {
         "board_name":board_name,
