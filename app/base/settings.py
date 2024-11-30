a89490488ff3f130ddf3aa8421a6815aae277f5a
mseo39 <a01063294905@gmail.com>
2024-11-30 16:39:55 +0900

    refactor: env.JWT변수 구분

--- app/base/settings.py
+++ app/base/settings.py
@@ -3,9 +3,9 @@ from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
     DATABASE_URL: str
-    SECRET_KEY: str
-    ALGORITHM: str
-    ACCESS_TOKEN_EXPIRE_MINUTES: int
+    JWT_SECRET_KEY: str
+    JWT_ALGORITHM: str
+    JWT_ACCESS_TOKEN_EXPIRE_MINUTES: int
 
     class Config:
         env_file = ".env"
--- app/utils/jwt.py
+++ app/utils/jwt.py
@@ -1,5 +1,6 @@
 from datetime import datetime, timedelta, timezone
 from jose import jwt
+
 from app.base.settings import settings
 
 
@@ -9,9 +10,9 @@ def create_access_token(data: dict[str, str]) -> str:
 
     KST = timezone(timedelta(hours=9))
 
-    expire = datetime.now(KST) + timedelta(minutes=settings.ACCESS_TOKEN_EXPIRE_MINUTES)
+    expire = datetime.now(KST) + timedelta(minutes=settings.JWT_ACCESS_TOKEN_EXPIRE_MINUTES)
     to_encode.update({"exp": expire.isoformat()})
     encoded_jwt = jwt.encode(
-        to_encode, settings.SECRET_KEY, algorithm=settings.ALGORITHM
+        to_encode, settings.JWT_SECRET_KEY, algorithm=settings.JWT_ALGORITHM
     )
     return encoded_jwt
