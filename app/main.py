30f77496c1b94616719ec8342e6d149929c979d7
mseo39 <a01063294905@gmail.com>
2024-11-30 17:42:18 +0900

    feat: cors 추가

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 import uvicorn
 from typing import Any, Dict
 
@@ -6,6 +7,13 @@ from app.domain.board.router import router as board_router
 
 app = FastAPI()
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True, 
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 app.include_router(router=board_router)
 
 
