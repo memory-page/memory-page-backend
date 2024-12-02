e6dc1aebf1de6cb8fb383b9121f9fc0ff1a8696b
LeeInGyu <developLee20@gmail.com>
2024-12-02 13:57:33 +0900

    Test: 칠판 생성 칠판 이름 정책 관련 테스트 코드 작성

--- app/tests/mock/mock_board.py
+++ app/tests/mock/mock_board.py
@@ -1,8 +1,9 @@
-from datetime import datetime, timedelta
-import uuid
 from httpx import AsyncClient, ASGITransport, Response
-import dataclasses
+from datetime import datetime, timedelta
+from unittest.mock import patch
 from typing import Tuple
+import dataclasses
+import uuid
 
 from app.main import app
 
@@ -21,6 +22,22 @@ async def mock_create_board(
     bg_num: int = 0,
     graduated_at: datetime | None = None,
 ) -> Tuple[Response, mock_board_dto]:
+    """
+    칠판 생성 모킹 함수
+
+    Parameter
+    ---
+    board_name: str | None = None, 칠판 이름
+    password: str | None = None, 비밀번호
+    bg_num: int = 0, 배경 번호
+    graduated_at: datetime | None = None, 졸업 날짜
+
+    Return
+    ---
+    Response: Response, request 결과
+    mock_board_dto: mock_board_dto, 생성된 칠판 정보
+    """
+    is_mock_board_name = board_name is None
     board_name = board_name or str(uuid.uuid4())
     password = password or str(uuid.uuid4())
     graduated_at = graduated_at or datetime.now() + timedelta(weeks=1)
@@ -35,7 +52,11 @@ async def mock_create_board(
     async with AsyncClient(
         transport=ASGITransport(app=app), base_url="http://test"
     ) as client:
-        response = await client.post("/board", json=mock_request)
+        if is_mock_board_name:
+            with patch("app.domain.board.service.BoardService._length_checker"):
+                response = await client.post("/board", json=mock_request)
+        else:
+            response = await client.post("/board", json=mock_request)
 
     mock_dto = mock_board_dto(
         board_name=board_name,
