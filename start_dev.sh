58361ba3606a4cb7cdf86db715042073ba81f165
LeeInGyu <developLee20@gmail.com>
2024-12-27 22:55:04 +0900

    Chore: prod 자동배포 설정

--- start_dev.sh
+++ start_dev.sh
@@ -8,18 +8,17 @@ echo "$(date): 배포 시작"
 
 # 기존 실행 중인 Gunicorn 프로세스 종료
 echo "기존 웹 서버 프로세스를 종료합니다..."
-pkill -f "gunicorn" || echo "기존 프로세스가 실행 중이 아닙니다."
+pkill -f "gunicorn-dev" || echo "기존 프로세스가 실행 중이 아닙니다."
 
 # 새로운 Gunicorn 서버 실행
 echo "새로운 웹 서버를 시작합니다..."
 
 /home/hadoop/anaconda3/bin/poetry install --no-root
 /home/hadoop/anaconda3/bin/poetry run gunicorn app.main:app \
+    --name gunicorn-dev \
     -w 7 \
     -k uvicorn.workers.UvicornWorker \
     --bind 0.0.0.0:5012 \
-    --certfile ./selfsigned.crt \
-    --keyfile ./selfsigned.key \
     --access-logfile ./gunicorn-access.log \
     --error-logfile ./gunicorn-error.log \
     --capture-output \
@@ -28,7 +27,7 @@ echo "새로운 웹 서버를 시작합니다..."
 
 # 서버 실행 성공 여부 확인
 sleep 5
-if pgrep -f "gunicorn" > /dev/null; then
+if pgrep -f "gunicorn-dev" > /dev/null; then
     echo "웹 서버가 성공적으로 실행되었습니다."
 else
     echo "웹 서버 실행 실패. 로그를 확인하세요."
